c4b23805eef68af6f78e73b4967c0e0dddafde14
dankarger <karger.dan@gmail.com>
2021-12-16 15:23:14 +0200

    test netlify

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -8,7 +8,7 @@ class Test extends React.Component {
     render(){
         return (
             <>
-                <h1>Test</h1>
+                <h1>TestyTest</h1>
             </>
         )
     }
